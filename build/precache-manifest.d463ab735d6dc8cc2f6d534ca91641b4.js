f625f991624c7b1f313a925737c3f22b0f2824e7
Yair Hadad <42780641+kuyu12@users.noreply.github.com>
2020-02-24 20:37:09 +0200

    upload build

---
+++ build/precache-manifest.d463ab735d6dc8cc2f6d534ca91641b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb05e5819938ae208f4faedb932dfd7",
+    "url": "/Tableau-Extension-Export-Naya/build/index.html"
+  },
+  {
+    "revision": "407bbd90acc8a34531d2",
+    "url": "/Tableau-Extension-Export-Naya/build/static/css/main.7bdf0d04.chunk.css"
+  },
+  {
+    "revision": "caa0c740105f60602089",
+    "url": "/Tableau-Extension-Export-Naya/build/static/js/2.9c2f2fa8.chunk.js"
+  },
+  {
+    "revision": "407bbd90acc8a34531d2",
+    "url": "/Tableau-Extension-Export-Naya/build/static/js/main.4bccb9dc.chunk.js"
+  },
+  {
+    "revision": "7d60c2f844e37fc9fd8f",
+    "url": "/Tableau-Extension-Export-Naya/build/static/js/runtime-main.9b4d6078.js"
+  }
+]);
